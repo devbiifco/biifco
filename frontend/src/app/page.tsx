cd9a0c4074d297801647bbcd1c6e98ca5c200fa0
devbiifco <dev@biffco.com>
2024-08-06 15:40:14 -0300

    various changes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
       hola mundo
+      hola
+      <div>
+        en esta zona iria el footer
+      </div>
     </main>
   );
 }
