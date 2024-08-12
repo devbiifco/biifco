4e5a360b60f0b6ca6269cb70cec44ce7a620fbb8
devbiifco <dev@biffco.com>
2024-08-12 12:02:02 -0300

    ipdate header config and style

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,7 +6,6 @@ import { textFont } from "../config/fonts";
 
 import type { AppProps } from 'next/app';
 import Header from '@/components/ui/header';
-import '../styles/globals.css'; 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
