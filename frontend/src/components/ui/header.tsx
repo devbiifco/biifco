fc2ab1b26f21934de6fd7ac3267a8712dbf74cd2
devbiifco <dev@biffco.com>
2024-08-14 18:53:47 -0300

    update 1.0.4

--- frontend/src/components/ui/header.tsx
+++ frontend/src/components/ui/header.tsx
@@ -1,59 +1,70 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import TextButton from './textButton';
-import PrimaryButton from './primaryButton';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
-// Componente Header
 const Header: React.FC = () => {
-  return (
-    <header className="bg-white w-full fixed top-0 left-0 z-10">
-      <div className="container mx-auto flex justify-between items-center p-8">
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-        {/* Logo de la marca */}
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header className="bg-white  w-full fixed top-0 left-0 z-10">
+      <div className="container mx-auto flex justify-between items-center p-8 py-4">
         <div className="flex items-center">
-          <Link href="/" legacyBehavior>
-            <a>
-              <Image src="/biifco.svg" alt="biifco" width={130} height={32} />
-            </a>
-          </Link>
+          <div >
+          <Image src="/biifco.svg" alt="biifco" width={130} height={32} />
+          </div>
+        <div className="container mx-auto flex justify-between items-center p-8 py-4">
+        {/* Menú de navegación para pantallas grandes */}
+        <nav className="hidden md:flex space-x-4 text-sm">
+          <a href="#home" className="hover:bg-gray-100 py-1 px-4 rounded-full transition-all duration-300">Product</a>
+          <a href="#about" className="hover:bg-gray-100 py-1 px-4 rounded-full transition-all duration-300">Solutions</a>
+          <a href="#services" className="hover:bg-gray-100 py-1 px-4 rounded-full transition-all duration-300">Resources</a>
+          <a href="#blog" className="hover:bg-gray-100 py-1 px-4 rounded-full transition-all duration-300">Docs</a>
+          <a href="#contact" className="hover:bg-gray-100 py-1 px-4 rounded-full transition-all duration-300">Pricing</a>
+        </nav>
+        </div>
+        </div>
+        {/* Botones de autenticación para pantallas grandes */}
+        <div className="hidden md:flex space-x-4 text-sm">
+          <a href="/login" className="hover:bg-gray-100 py-1 px-4 rounded-full transition-all duration-300">Login</a>
+          <a href="#signup" className="rounded bg-gray-100 text-gray-800 hover:text-white hover:bg-blue-600 py-1 px-4 rounded-full transition-all duration-300">Contact</a>
+          <a href="#signup" className="rounded bg-gray-100 text-gray-800 hover:text-white hover:bg-blue-600 py-1 px-4 rounded-full transition-all duration-300">Sign Up</a>
         </div>
 
-        {/* Navegación principal */}
-        <nav className="flex space-x-4 text-sm">
-          <Link href="#Products" legacyBehavior>
-            <TextButton href="#Products">Products</TextButton>
-          </Link>
-          <Link href="#Solutions" legacyBehavior>
-            <TextButton href="#Solutions">Solutions</TextButton>
-          </Link>
-          <Link href="#Resources" legacyBehavior>
-            <TextButton href="#Resources">Resources</TextButton>
-          </Link>
-          <Link href="#Docs" legacyBehavior>
-            <TextButton href="#Docs">Docs</TextButton>
-          </Link>
-          <Link href="#Pricing" legacyBehavior>
-            <TextButton href="#Pricing">Pricing</TextButton>
-          </Link>
-        </nav>
-
-        {/* Sección de botones de acciones (Login, Contact, Sign Up) */}
-        <div className="flex space-x-4 text-sm">
-          <Link href="/login" legacyBehavior>
-            <PrimaryButton href="/login">Login</PrimaryButton>
-          </Link>
-          <Link href="#contact" legacyBehavior>
-            <PrimaryButton href="#contact">Contact</PrimaryButton>
-          </Link>
-          <Link href="#signup" legacyBehavior>
-            <PrimaryButton href="#signup">Sign Up</PrimaryButton>
-          </Link>
+        {/* Menú hamburguesa para pantallas pequeñas */}
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="text-gray-800 text-2xl focus:outline-none">
+            {isMenuOpen ? <FaTimes /> : <FaBars />}
+          </button>
         </div>
       </div>
+
+      {/* Menú desplegable para pantallas pequeñas */}
+      {isMenuOpen && (
+        <div className="md:hidden bg-white shadow-md w-full fixed top-16 left-0 z-10">
+          <nav className="flex flex-col items-center py-4 space-y-4">
+            <a href="#home" onClick={toggleMenu} className="hover:bg-gray-100 py-1 px-4 rounded-full transition-all duration-300">Product</a>
+            <a href="#about" onClick={toggleMenu} className="hover:bg-gray-100 py-1 px-4 rounded-full transition-all duration-300">Solutions</a>
+            <a href="#services" onClick={toggleMenu} className="hover:bg-gray-100 py-1 px-4 rounded-full transition-all duration-300">Resources</a>
+            <a href="#blog" onClick={toggleMenu} className="hover:bg-gray-100 py-1 px-4 rounded-full transition-all duration-300">Docs</a>
+            <a href="#contact" onClick={toggleMenu} className="hover:bg-gray-100 py-1 px-4 rounded-full transition-all duration-300">Pricing</a>
+
+            {/* Botones de autenticación */}
+            <div className="flex flex-col items-center space-y-2 pt-4">
+              <a href="/login" onClick={toggleMenu} className="bg-gray-100 text-gray-800 hover:bg-gray-200 py-1 px-4 rounded-full transition-all duration-300">Login</a>
+              <a href="#signup" onClick={toggleMenu} className="bg-blue-600 text-white hover:bg-blue-700 py-1 px-4 rounded-full transition-all duration-300">Sign Up</a>
+            </div>
+          </nav>
+        </div>
+      )}
     </header>
   );
 };
 
 export default Header;
-
