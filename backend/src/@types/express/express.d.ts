a527475545a016b6df50a8e07148cbe643a1052e
devbiifco <dev@biifco.com>
2024-08-23 17:55:38 -0600

    backend delete files

--- backend/src/@types/express/express.d.ts
+++
@@ -1,7 +0,0 @@
-import { IUser } from '../../models/user'; // Ajusta esta ruta si es necesario
-
-declare module 'express-serve-static-core' {
-  interface Request {
-    user?: IUser | null;  // Aquí definimos que `user` puede existir y ser de tipo `IUser`
-  }
-}
--- backend/src/@types/index.d.ts
+++
@@ -1,8 +0,0 @@
-// @types/express/index.d.ts
-import { IUser } from '../models/user';
-
-declare module 'express-serve-static-core' {
-  interface Request {
-    user?: IUser; // Ajusta el tipo según tu modelo de usuario
-  }
-}
