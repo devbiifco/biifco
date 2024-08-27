888968a0d52b4f7ebfc7f6ee28ffe72150c12ed9
devbiifco <dev@biifco.com>
2024-08-27 05:43:06 -0600

    backend & front ok 26/08

--- frontend/src/components/ui/header.tsx
+++ frontend/src/components/ui/header.tsx
@@ -1,24 +1,42 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Header: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Verificar si hay un token de sesión almacenado
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
+  const handleLogout = () => {
+    // Eliminar el token de sesión y cambiar el estado
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
+    // Redirigir a la home
+    window.location.href = '/';
+  };
+
   return (
     <header className="w-full fixed top-0 left-0 z-10">
       <div className="container mx-auto flex justify-between items-center p-8 py-4">
         <div className="flex items-center">
           <div>
-          <Link href="/">
-            <Image src="/biifco.svg" alt="biifco" width={130} height={32} />
-          </Link>
+            <Link href="/">
+              <Image src="/biifco.svg" alt="biifco" width={130} height={32} />
+            </Link>
           </div>
           <div className="container mx-auto flex justify-between items-center p-8 py-4">
             {/* Menú de navegación para pantallas grandes */}
@@ -31,11 +49,20 @@ const Header: React.FC = () => {
             </nav>
           </div>
         </div>
+
         {/* Botones de autenticación para pantallas grandes */}
         <div className="hidden md:flex space-x-4 text-sm">
-          <a href="/login" className="hover:bg-secondary py-1 px-4 rounded-full transition-all duration-300">Login</a>
-          <a href="#" className="rounded bg-secondary text-black hover:text-white hover:bg-primary py-1 px-4 rounded-full transition-all duration-300">Contact</a>
-          <a href="/signup" className="rounded bg-secondary text-black hover:text-white hover:bg-primary py-1 px-4 rounded-full transition-all duration-300">Sign Up</a>
+          {isLoggedIn ? (
+            <>
+              <button onClick={handleLogout} className="hover:bg-secondary py-1 px-4 rounded-full transition-all duration-300">Logout</button>
+              <a href="#" className="rounded bg-secondary text-black hover:text-white hover:bg-primary py-1 px-4 rounded-full transition-all duration-300">Contact</a>
+            </>
+          ) : (
+            <>
+              <a href="/login" className="hover:bg-secondary py-1 px-4 rounded-full transition-all duration-300">Login</a>
+              <a href="/signup" className="rounded bg-secondary text-black hover:text-white hover:bg-primary py-1 px-4 rounded-full transition-all duration-300">Sign Up</a>
+            </>
+          )}
         </div>
 
         {/* Menú hamburguesa para pantallas pequeñas */}
@@ -58,9 +85,18 @@ const Header: React.FC = () => {
 
             {/* Botones de autenticación */}
             <div className="flex flex-col items-center space-y-2 pt-4">
-              <a href="/login" className="hover:bg-accent py-1 px-4 rounded-full transition-all duration-300">Login</a>
-              <a href="#signup" className="rounded bg-secondary text-white hover:text-white hover:bg-primary py-1 px-4 rounded-full transition-all duration-300">Contact</a>
-              <a href="#signup" className="rounded bg-secondary text-white hover:text-white hover:bg-primary py-1 px-4 rounded-full transition-all duration-300">Sign Up</a>
+              {isLoggedIn ? (
+                <>
+                  <button onClick={handleLogout} className="hover:bg-accent py-1 px-4 rounded-full transition-all duration-300">Logout</button>
+                  <a href="#" className="rounded bg-secondary text-white hover:text-white hover:bg-primary py-1 px-4 rounded-full transition-all duration-300">Contact</a>
+                </>
+              ) : (
+                <>
+                  <a href="/login" onClick={toggleMenu} className="hover:bg-accent py-1 px-4 rounded-full transition-all duration-300">Login</a>
+                  <a href="#signup" onClick={toggleMenu} className="rounded bg-secondary text-white hover:text-white hover:bg-primary py-1 px-4 rounded-full transition-all duration-300">Contact</a>
+                  <a href="#signup" onClick={toggleMenu} className="rounded bg-secondary text-white hover:text-white hover:bg-primary py-1 px-4 rounded-full transition-all duration-300">Sign Up</a>
+                </>
+              )}
             </div>
           </nav>
         </div>
--- frontend/src/components/ui/signup.tsx
+++ frontend/src/components/ui/signup.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import { FaFacebook, FaGoogle, FaApple, FaUser, FaEnvelope, FaLock } from 'react-icons/fa';
+import axiosInstance from '../../utils/axiosInstance'; // Importa axiosInstance
 
 const Signup: React.FC = () => {
-  // Estado para almacenar los valores de los campos
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     password: ''
   });
 
-  // Manejar cambios en los inputs
+  const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState<string | null>(null);
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({
       ...formData,
@@ -17,18 +19,29 @@ const Signup: React.FC = () => {
     });
   };
 
-  // Manejar el envío del formulario
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    // Aquí puedes agregar la lógica para enviar los datos del formulario al backend
-    console.log('Datos del formulario:', formData);
+    try {
+      const response = await axiosInstance.post('/auth/register', {
+        email: formData.email,
+        password: formData.password
+      });
+      setSuccess('User registered successfully!');
+      setError(null);
+      console.log('Success:', response.data);
+    } catch (error) {
+      setError('Registration failed. Please try again.');
+      setSuccess(null);
+      console.error('Error:', error);
+    }
   };
 
   return (
     <div className="flex flex-col justify-center items-center flex-grow p-4">
-      {/* Título de la página de registro */}
       <h1 className="text-2xl font-bold mb-6">Sign up for biifco</h1>
-      
+      {/* Mensajes de éxito o error */}
+      {success && <div className="text-green-500">{success}</div>}
+      {error && <div className="text-red-500">{error}</div>}
       {/* Botones de registro con redes sociales */}
       <div className="flex flex-col space-y-4 mb-6">
         <button className="flex items-center space-x-5 px-12 py-2 border rounded-full bg-blue-400 text-white hover:bg-blue-600 transition-colors duration-300">
@@ -44,7 +57,6 @@ const Signup: React.FC = () => {
           <span>Continue with Apple</span>
         </button>
       </div>
-      
       {/* Formulario de registro */}
       <form onSubmit={handleSubmit} className="flex flex-col space-y-4 py-0 w-full max-w-sm">
         <div className="relative flex items-center ">
@@ -87,7 +99,6 @@ const Signup: React.FC = () => {
           Sign Up
         </button>
       </form>
-      
       {/* Enlaces para iniciar sesión y recuperación de contraseña */}
       <a href="/login" className="text-sm mt-4 hover:text-primary transition-colors duration-300">
         Already have an account? Log in
